c124f07a606e9bbc36923f6a1ea7a33d191f1338
byrnereese <byrne@majordojo.com>
2020-04-27 10:27:01 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.1'
+
+setup(
+    name="mkdocs-ringcentral",
+    version=VERSION,
+    url='https://github.com/byrnereese/mkdocs-ringcentral/',
+    description='RingCentral theme for MkDocs',
+    author='Byrne Reese',
+    author_email='byrne.reese@ringcentral.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'mkdocs>=1.1',
+        'mkdocs-moonstone>=0.1.8'
+    ],
+    python_requires='>=3.0',
+    entry_points={
+        'mkdocs.themes': [
+            'ringcentral = mkdocs_ringcentral',
+        ]
+    },
+    zip_safe=False
+)
