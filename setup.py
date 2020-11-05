61ff3af2dcb3791945a597e6fd86b4adb14d65df
byrnereese <byrne@majordojo.com>
2020-11-04 17:07:06 -0800

    Fixed and updated deps

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.8'
+VERSION = '0.1.9'
 
 setup(
     name="mkdocs-ringcentral",
@@ -14,7 +14,7 @@ setup(
     install_requires=[
         'mkdocs>=1.1',
         'mkdocs-ringcentral-api-index>=0.1.2',
-        'mkdocs-moonstone>=0.1.15'
+        'mkdocs-moonstone>=0.1.16'
     ],
     python_requires='>=3.0',
     entry_points={
