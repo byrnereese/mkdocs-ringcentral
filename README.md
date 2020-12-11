7eea03690fe405b40d3247d6389f7c3bc15d0c15
Byrne Reese <byrne@majordojo.com>
2020-12-11 13:26:09 -0800

    Added README

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a proprietary theme used to power RingCentral developer documentation. 
+
+The RingCentral logo is trademarked by RingCentral. Usage without express permission from RingCentral is prohibited. 
--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 
 VERSION = '0.1.12'
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="mkdocs-ringcentral",
     version=VERSION,
     url='https://github.com/byrnereese/mkdocs-ringcentral/',
     description='RingCentral theme for MkDocs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Byrne Reese',
     author_email='byrne.reese@ringcentral.com',
     packages=find_packages(),
